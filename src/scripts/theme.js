7b15b59c4ad3fa88bfee23f754313d8b63e28db5
Mohammed Thahir <mohammed.thahir@chalhout.com>
2021-07-15 02:56:08 +0400

    Added related product using custom fields app+liquid

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -4,13 +4,14 @@ import Currency from './money-format';
 import MediaGallery from './pdp/master-gallery-slider';
 import PDPListeners from './pdp/pdp-listeners';
 import HeaderListeners from './listeners/header-listeners';
-
+import RelatedProductSlider from './pdp/related-product-slider';
 import SparxScripts from './sparx/sparx-scripts'
 
 window.Cart = cart;
 
 document.addEventListener('DOMContentLoaded', function() {
   MediaGallery.init();
+  RelatedProductSlider.init();
   SparxScripts.init();
   PDPListeners.init();
   HeaderListeners.init();
